abd04a73d0e8206d2554f717eb9bf5fb7fc454b9
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-03 11:13:20 +0100

    Updates

---
+++ precache-manifest.5761d0e38168f8d9a82e6da9d184a9e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d2328efb782a5de3aa6353b985a942",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "3b51144e82c916fcf1fe",
+    "url": "/todo-app/static/js/2.03150a73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.03150a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ddea8baa38c0207a08",
+    "url": "/todo-app/static/js/main.4577448a.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
